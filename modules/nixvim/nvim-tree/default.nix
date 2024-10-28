57db6d69b85f6582a474194cfbda0577e42a867f
Justin Martin <jus10mar10@gmail.com>
2024-10-27 20:39:03 -0500

    update keymaps

--- modules/nixvim/nvim-tree/default.nix
+++ modules/nixvim/nvim-tree/default.nix
@@ -1,4 +1,5 @@
-{helpers, ...}: {
+{ helpers, ... }:
+{
   keymaps = [
     {
       mode = [
@@ -6,9 +7,9 @@
         "v"
       ];
       action = "<cmd>NvimTreeToggle<CR>";
-      key = "<C-n>";
+      key = "<leader>e";
       options = {
-        desc = "nvim-tree: Toggle";
+        desc = "nvim-tree: Focus";
         silent = true;
         noremap = true;
       };
@@ -41,24 +42,24 @@
 
     onAttach =
       helpers.mkRaw # lua
-      
-      ''
-        function(bufnr)
-          local api = require "nvim-tree.api"
 
-          local function opts(desc)
-            return { desc = "nvim-tree: ", buffer = bufnr, noremap = true, silent = true }
+        ''
+          function(bufnr)
+            local api = require "nvim-tree.api"
+
+            local function opts(desc)
+              return { desc = "nvim-tree: ", buffer = bufnr, noremap = true, silent = true }
+            end
+
+            api.config.mappings.default_on_attach(bufnr)
+
+            vim.keymap.set("n", "s", api.node.open.vertical, opts("Open (Vertical)"))
+            vim.keymap.set("n", "t", api.node.open.tab, opts("Open (Tab)"))
+            vim.keymap.set("n", "S", api.node.open.horizontal, opts("Open (Horizontal)"))
+            vim.keymap.set("n", "O", api.node.run.system, opts("Open (System)"))
+            vim.keymap.set("n", "<C-r>", api.tree.reload, opts("Reload"))
+            vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
           end
-
-          api.config.mappings.default_on_attach(bufnr)
-
-          vim.keymap.set("n", "s", api.node.open.vertical, opts("Open (Vertical)"))
-          vim.keymap.set("n", "t", api.node.open.tab, opts("Open (Tab)"))
-          vim.keymap.set("n", "S", api.node.open.horizontal, opts("Open (Horizontal)"))
-          vim.keymap.set("n", "O", api.node.run.system, opts("Open (System)"))
-          vim.keymap.set("n", "<C-r>", api.tree.reload, opts("Reload"))
-          vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
-        end
-      '';
+        '';
   };
 }
