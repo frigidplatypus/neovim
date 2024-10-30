a83a86d646a50a694f31cf3d9a3fef9b3f8fa841
Justin Martin <jus10mar10@gmail.com>
2024-10-30 07:24:17 -0500

    add lazygit

--- modules/nixvim/lazygit/default.nix
+++ modules/nixvim/lazygit/default.nix
@@ -1,1 +1,12 @@
-{ plugins.lazygit.enable = true; }
+{
+  plugins.lazygit.enable = true;
+  keymaps = [
+    {
+      key = "<leader>gg";
+      action = "<cmd>LazyGit<cr>";
+      options = {
+        desc = "Open LazyGit";
+      };
+    }
+  ];
+}
