8c85a3748ea0f0943325e559971d97728c6dacee
Justin Martin <jus10mar10@gmail.com>
2024-11-14 20:38:32 -0600

    reformat

--- modules/nixvim/lsp/default.nix
+++ modules/nixvim/lsp/default.nix
@@ -9,12 +9,12 @@
   ];
 
   extraConfigLua = ''
-    			vim.api.nvim_create_autocmd("LspAttach", {
-      callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        client.server_capabilities.semanticTokensProvider = nil
-      end,
-    });
+    	vim.api.nvim_create_autocmd("LspAttach", {
+       callback = function(args)
+         local client = vim.lsp.get_client_by_id(args.data.client_id)
+         client.server_capabilities.semanticTokensProvider = nil
+       end,
+     });
   '';
 
   autoCmd = [
