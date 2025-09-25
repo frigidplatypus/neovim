f4ab643ac9a9d4dc7615f303b40557e949705541
Western Admin <it@westerncusd12.org>
2025-09-25 08:27:09 -0500

    add seperate package for kanagawa colorscheme

---
+++ packages/neovim-kanagawa/default.nix
@@ -0,0 +1,14 @@
+{ lib, pkgs, inputs, ... }:
+
+# Thin wrapper around the main `neovim` package that only changes the colorscheme.
+import ../neovim {
+  inherit lib pkgs inputs;
+
+  # Only change: disable the default and enable Kanagawa
+  neovim-config = lib.mkForce {
+    colorschemes = {
+      gruvbox = { enable = false; };
+      kanagawa = { enable = true; };
+    };
+  };
+}
