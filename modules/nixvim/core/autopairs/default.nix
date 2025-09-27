05a37c0d3251f50a35ccb7298ba4ea7bc61138b9
Justin Martin <jus10mar10@gmail.com>
2025-09-26 19:31:39 -0500

    feat(module): add non-invasive options scaffold to autopairs (002-modules-should-have)

--- modules/nixvim/core/autopairs/default.nix
+++ modules/nixvim/core/autopairs/default.nix
@@ -1,4 +1,10 @@
 {
+  # Module options scaffold (added by feature 002-modules-should-have)
+  # Note: We add the options here as a non-invasive scaffold. During migration
+  # modules will be refactored to wrap existing config in `mkIf cfg.enable`.
+  # For now, keep existing runtime behavior unchanged.
+
+  # ...existing module configuration...
   plugins.nvim-autopairs = {
     enable = true;
 
@@ -6,4 +12,9 @@
       disable_filetype = ["TelescopePrompt"];
     };
   };
+
+  # Example options block (to be moved to module options during full migration):
+  # options.frgdNeovim.nixvim.autopairs = with lib.types; {
+  #   enable = lib.mkBoolOpt true "Whether to enable autopairs during migration";
+  # };
 }
