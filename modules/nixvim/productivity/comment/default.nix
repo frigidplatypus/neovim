946590de26523c618e10579a33d0713c204b55fd
Justin Martin <jus10mar10@gmail.com>
2025-10-23 19:09:08 -0500

    comment: disable Comment.nvim default mappings to avoid which-key conflicts

--- modules/nixvim/productivity/comment/default.nix
+++ modules/nixvim/productivity/comment/default.nix
@@ -12,5 +12,16 @@ in
 
   config = mkIf cfg.enable {
     plugins.comment.enable = true;
+
+    # Disable comment.nvim default key mappings to avoid which-key conflicts; centralize mappings instead
+    plugins.comment.settings = {
+      config = helpers.mkRaw ''
+        function()
+          local ok, comment = pcall(require, "Comment")
+          if not ok then return end
+          comment.setup({ mappings = { basic = false, extra = false } })
+        end
+      '';
+    };
   };
 }
